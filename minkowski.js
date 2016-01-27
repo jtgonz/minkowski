358c23d2ba9d7e535539716b889ac0c5e822fd90
Jesse T. Gonzalez <jtgonz1@gmail.com>
2016-01-27 16:09:49 -0500

    function to transform instants from one reference frame to another

--- minkowski.js
+++ minkowski.js
@@ -53,6 +53,33 @@ function render_objects (frame, objects, x, t) {
       .style("fill", 'black');
 }
 
+// Given an instant in some particle's worldline. Find how that instant looks  in
+// the reference frame of another particle at a certain time in the life of that
+// particle. Returns a transformed instant.
+function transform (instant, particle, time)
+  // search world line of particle until we reach an event that occurs either now
+  // or in the "past" (when the particle's age/clock was smaller than the time
+  // provided). This event is the origin of the particle's current reference frame.
+  let origin = particle.reduce( (prev, curr) => curr.clock <= time ? curr : prev)
+
+  // get e-prime: shift coordinates and apply Lorentz boost 
+  let e_pr = boost(instant.e.x - origin.e.x, instant.e.t - origin.e.t, origin.v);
+
+  // get v-prime: relativisitc velocity transformation
+  let v_pr = (instant.v - origin.v) / (1 - instant.v * origin.v)
+
+  // This is how the instant appears in the particle's reference frame, when the
+  // particle is at the given time in it's own frame.
+  return { v: v_prime, e: e_prime, clock: instant.clock }
+}
+
+// Lorentz boost with c = 1
+function boost (x, t, v) {
+  let gamma = 1 / Math.sqrt(1 - v * v)    // Lorentz factor
+  return {x: (x - v * t) * gamma, t: (t - v * x) * gamma}
+}
+
+
 /* ***************************************************************************** */
 
 window.onload = function() {
@@ -70,14 +97,32 @@ let line = d3.svg.line()
 
 draw_axes(frame, line);
 
-// create objects and add objects to world
-let objects = [
-  {x:0, t:0, id:0}, {x:0.7, t:0, id:1}, {x:-0.2, t:0, id:2}, {x:-0.5, t:0, id:3}
-];
-//place_object();
-
-//update_objects(objects)
-render_objects(frame, objects, x, t);
-
-
 };
+
+/*
+
+A particle looks like this:
+
+particle = [
+  { v: velocity_in_R, e: coords_in_R, clock: age_of_particle },
+  { v: velocity_in_R, e: coords_in_R, clock: age_of_particle },
+  { v: velocity_in_R, e: coords_in_R, clock: age_of_particle } ...
+]
+
+This is essentially the particle's world line. Each element in the list is a
+different instant in the life of that particle.
+
+- e is the event in the coordinates of R, the stationary reference frame.
+- v is the velocity of the particle relative to R, at that particular instant.
+- clock is the age of the particle (how it experiences time at that event)
+
+So for the user, the general viewing pattern looks like this:
+1. Increment clock.
+2. Create new event for user if necessary (user just tried to change velocity)
+    and add to worldline of particle.
+3. Travel along worldline of each particle, transforming each event into the viewer
+    reference frame. Look at the most recent transformed event. Grab the 
+    transformed velocity and figure out where the particle should be plotted.
+4. Plot particles! aaaaand we're done.
+
+*/
