7fbf77d20f2b0cb2562a9b6fcc1bc3c094ffd802
Jesse T. Gonzalez <jtgonz1@gmail.com>
2016-01-27 22:29:05 -0500

    time dilation seems to be working

--- minkowski.js
+++ minkowski.js
@@ -3,6 +3,12 @@
 // define (0,0) and (1,1) in terms of display coordinates 
 let [x0, y0, x1, y1] = [200, 200, 400, 0];
 
+// how often should we update the display? (in milliseconds)
+let update_interval = 40;
+
+// how fast should the user age?
+let clock_speed = 0.02
+
 /* ***************************************************************************** */
 
 // create scale to map minkowski coordinates to display coordinates
@@ -37,13 +43,13 @@ function set_frame_width (frame, w, h) {
   return frame.attr('width', w).attr('height', h)
 }
 
-function render_objects (frame, objects, x, t) {
-  return frame.selectAll('.object')
-      .data(objects, d => d.id)
+function render_particles (frame, particles, x, t) {
+  frame.selectAll('.particle')
+      .data(particles, d => d.id)
       .attr('transform', d => 'translate(' + x(d.x) + ',' + t(d.t) + ')')
     .enter()
       .append('g')
-      .attr('class', 'object')
+      .attr('class', 'particle')
       .attr('transform', d => 'translate(' + x(d.x) + ',' + t(d.t) + ')')
       .append('circle')
       .attr('transform', 'translate(0, -10)')
@@ -51,16 +57,18 @@ function render_objects (frame, objects, x, t) {
       .attr("cy", 0)
       .attr("r", 10)
       .style("fill", 'black');
+  return frame;
 }
 
 // Given an instant in some particle's worldline. Find how that instant looks  in
 // the reference frame of another particle at a certain time in the life of that
 // particle. Returns a transformed instant.
-function transform (instant, particle, time)
+function transform (instant, particle, time) {
   // search world line of particle until we reach an event that occurs either now
   // or in the "past" (when the particle's age/clock was smaller than the time
   // provided). This event is the origin of the particle's current reference frame.
-  let origin = particle.reduce( (prev, curr) => curr.clock <= time ? curr : prev)
+  let origin = particle.worldline.reduce( (prev, curr) =>
+    curr.clock <= time ? curr : prev);
 
   // get e-prime: shift coordinates and apply Lorentz boost 
   let e_pr = boost(instant.e.x - origin.e.x, instant.e.t - origin.e.t, origin.v);
@@ -70,7 +78,7 @@ function transform (instant, particle, time)
 
   // This is how the instant appears in the particle's reference frame, when the
   // particle is at the given time in it's own frame.
-  return { v: v_prime, e: e_prime, clock: instant.clock }
+  return { v: v_pr, e: e_pr, clock: instant.clock }
 }
 
 // Lorentz boost with c = 1
@@ -79,14 +87,64 @@ function boost (x, t, v) {
   return {x: (x - v * t) * gamma, t: (t - v * x) * gamma}
 }
 
+function update () {
+  // advance state; update clock, frame, particle, and queue
+  update.state = advance_state(update.state);
+}
+
+function advance_state (state) {
+  let clock = state.clock + clock_speed;  // increment clock
+
+  // get particle that corresponds to viewer's reference frame (given by id)
+  let particles = state.particles;
+  let id = state.id;
+  let particle_ref = particles.find( particle => particle.id == id );
+
+  // TODO: look at event queue and create new events
+  let queue = state.queue;
+
+  // Travel along the worldline of each particle, transforming each event into
+  // the viewer's reference frame. For each particle, grab the most recent
+  // transformed event (where the t coordinate in less than or equal to the current
+  // clock). Then use that transformed event to find the x position of particle
+  let particles_coords = particles.map( particle => {
+    return {
+      id: particle.id,
+      instant: particle.worldline.reduce( (prev, curr) =>
+        transform(curr, particle_ref, clock).e.t <= clock ? curr : prev)
+    }
+  }).map( reduced_particle => {
+    // transform particle into the viewer's reference frame
+    let instant = reduced_particle.instant
+    let instant_p = transform(instant, particle_ref, clock);
+
+    // calculate delta t between recent instant and current time. then find current
+    // position of particle.
+    let delta_t = clock - instant_p.e.t;
+
+    return {
+      id: reduced_particle.id + "_now", 
+      x: instant_p.e.x + instant_p.v * delta_t,
+      t: 0
+    }
+  });
+
+  let frame = state.frame;
+/*
+  frame.selectAll('g.worldline').datum( d => {
+    d.hist.map(a => a - clock_speed).push(particles_now[d.id])
+    // check to see if history is too long, and remove elements if so
+  })
+*/
+
+  // render objects to frame using x and t scales
+  frame = render_particles(frame, particles_coords, x, t);
+
+  return {id: id, clock: clock, frame: frame, particles: particles, queue: queue}
+
+}
 
 /* ***************************************************************************** */
-
-window.onload = function() {
-
-// get svg object representing user's reference frame
-let frame = d3.select('svg#frame');
-set_frame_width(frame, x0 * 2, y0 * 2);
 
 // set scale and create a line generator. 'line' will generate path data in
 // display coordinates when given an array in minkowski coordinates.
@@ -95,7 +153,28 @@ let line = d3.svg.line()
     .x( d => x(d.x) )
     .y( d => t(d.t) );
 
+window.onload = function() {
+
+// get svg object representing user's reference frame
+let frame = d3.select('svg#frame');
+set_frame_width(frame, x0 * 2, y0 * 2);
+
 draw_axes(frame, line);
+
+// create particle with one instant in worldline
+let particle = {id: 0, worldline: [ {v: -0.2, e: {x: 0, t: 0}, clock: 0} ]};
+let particle_b = {id: 1, worldline: [ {v: -0.2, e: {x: -0.2, t: 0}, clock: 0} ]};
+let particle_c = {id: 2, worldline: [
+  {v: 0.2, e: {x: 0, t: 0}, clock: 0},
+  {v: -0.3, e: {x: 0.4, t: 2}, clock: 0} ]};
+let particle_d = {id: 3, worldline: [
+  {v: 0, e: {x: 0.2, t: 0}, clock: 0},
+  {v: 0, e: {x: 10, t: 0.2}, clock: 0} ]};
+let particles = [ particle, particle_d ];
+
+// set initial state, and begin to travel through worldline of particle
+update.state = {id: 0, clock: 0, frame: frame, particles: particles, queue: []}
+window.setInterval(update, update_interval);
 
 };
 
@@ -103,11 +182,14 @@ draw_axes(frame, line);
 
 A particle looks like this:
 
-particle = [
-  { v: velocity_in_R, e: coords_in_R, clock: age_of_particle },
-  { v: velocity_in_R, e: coords_in_R, clock: age_of_particle },
-  { v: velocity_in_R, e: coords_in_R, clock: age_of_particle } ...
-]
+particle = {
+  id: particle_id,
+  worldline: [
+    { v: velocity_in_R, e: coords_in_R, clock: age_of_particle },
+    { v: velocity_in_R, e: coords_in_R, clock: age_of_particle },
+    { v: velocity_in_R, e: coords_in_R, clock: age_of_particle } ...
+  ]
+}
 
 This is essentially the particle's world line. Each element in the list is a
 different instant in the life of that particle.
